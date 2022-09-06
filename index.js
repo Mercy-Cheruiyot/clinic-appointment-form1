215d835acb073f80ce90bf6571aca8cfe7caf265
Mercy <mercycheruiyot2013@gmail.com>
2022-09-06 11:49:46 +0300

    Adding the eye video

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", ()=>{
       document.getElementById('myform').addEventListener("submit",(e)=>{
          e.preventDefault();
-        alert("I've been submitted.");
+        alert(" Submitted.");
      })
    });
  
