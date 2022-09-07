b54f33b4c0a5bab5c0eb510ba994a15797b239a8
Mercy <mercycheruiyot2013@gmail.com>
2022-09-07 08:43:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -4,4 +4,19 @@ document.addEventListener("DOMContentLoaded", ()=>{
         alert(" Submitted.");
      })
    });
+   const x=document.getElementById("navbar");
+x.addEventListener("click",register);
+ function register(){
+alert("Register Here!");
+   
+}
+function showHide(){
+   const myform1= document.getElementById("myform1");
+   if(myform1.style.display==="none"){
+      myform1.style.display="block";
+   }
+   else{
+      myform1.style.display="none";
+   }
+};
  
